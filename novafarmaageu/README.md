15903ce3b3682e178caf044cf7b15a95e538059b
lucasduartepontes01-pixel <lucasduartepontes01@gmail.com>
2025-11-12 18:58:59 -0300

    Add files via upload

---
+++ novafarmaageu/README.md
@@ -0,0 +1,3 @@
+# Nova Farma Ageu v3.1
+
+Realtime fixes, products catalog, uppercase names, export CSV/PDF.
---
+++ novafarmaageu/src/firebase.js
@@ -0,0 +1,5 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = { apiKey: 'AIzaSyBW6ZTGHyaYwVgT9dog2y-B2Xg44d_047o', authDomain: 'novafarmaageu-5c519.firebaseapp.com', projectId: 'novafarmaageu-5c519', storageBucket: 'novafarmaageu-5c519.firebasestorage.app', messagingSenderId: '954166915495', appId: '1:954166915495:web:d90f95364e95c8e31b0682', measurementId: 'G-XNDV0NKV3H' };
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
